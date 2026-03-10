0d7ccd35fdb61400e37d21104457ca9cd55f99ec
sanket3646 <sanketkakade32@gmail.com>
2026-03-10 09:18:37 +0530

    prepare for netlify

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "https://hctvopeouuimekbbrfhp.supabase.co"
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhjdHZvcGVvdXVpbWVrYmJyZmhwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMwNzM0MjgsImV4cCI6MjA4ODY0OTQyOH0.V2nOevdB6Wurz6iZy1vivCBYI1chLq7TCGmNqTowKTk"
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
